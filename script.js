6f4f5d6a6e5f153010807bd28dfbb1a7b82d9010
AnndreJuan <anndret26@gmail.com>
2024-07-27 18:29:35 -0300

    mobile version

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ document.querySelectorAll('.hero_events_link').forEach(link => {
     });
 });
 
-const heroLinks = document.querySelector('.hero_links');
-const hero_events = document.querySelector('.hero_events');
-const hero_info = document.querySelector('.hero_info');
+// const heroLinks = document.querySelector('.hero_links');
+// const hero_events = document.querySelector('.hero_events');
+// const hero_info = document.querySelector('.hero_info');
 
-function desativateScroll () {
-    if (window.scrollY > 120) {
-        heroLinks.style.display = 'none';
-    } else {
-        heroLinks.style.display = 'Flex';
-    };
+// function desativateScroll () {
+//     if (window.scrollY > 120) {
+//         heroLinks.style.display = 'none';
+//     } else {
+//         heroLinks.style.display = 'Flex';
+//     };
 
-    if (window.scrollY > 160) {
-        hero_events.style.display = 'none';
-    } else {
-        hero_events.style.display = 'Flex';
-    };
+//     if (window.scrollY > 160) {
+//         hero_events.style.display = 'none';
+//     } else {
+//         hero_events.style.display = 'Flex';
+//     };
 
-    if (window.scrollY > 200) {
-        hero_info.style.display = 'none';
-    } else {
-        hero_info.style.display = 'Flex';
-    };
-};
+//     if (window.scrollY > 200) {
+//         hero_info.style.display = 'none';
+//     } else {
+//         hero_info.style.display = 'Flex';
+//     };
+// };
 
-window.addEventListener('scroll', desativateScroll);
+// window.addEventListener('scroll', desativateScroll);
